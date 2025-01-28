b0547c9267a9ea924dfef2982a4f81a9c044d4cc
AbdelrahmanSuliman <abdelrahmansuliman123@gmail.com>
2025-01-28 22:23:29 +0200

    MY FIRST COMMIT HAHAHA :)

--- task1/main.c
+++ task1/main.c
@@ -46,10 +46,9 @@ void change_directory(char *path){
     char cwd[1024];
 
     if(path == NULL){
-       char* cwd = getenv("PWD");
-        if(cwd){
-            printf("%s\n", cwd);
-        }
+      if(getcwd(cwd, sizeof(cwd)) != NULL){
+        printf("%s\n", cwd);
+    }
         return;
     }
 
@@ -71,108 +70,197 @@ void change_directory(char *path){
 }
 
 
-void execute_command(char** words){
-    pid_t pid = fork();
-
-    if(pid < 0) {
-        perror("fork failed");
-        return;
-    }
-    else if(pid == 0) { 
-        if(execvp(words[0], words) == -1) {
-            perror("command execution failed");
-            exit(EXIT_FAILURE);
-        }
-    }
-    else {  
-        waitpid(pid, NULL, 0);
-    }
-}
-
-int main(){
-    chdir("/");
-    setenv("PWD", "/", 1);
+void process_input(FILE* input_source) {
     while(1){
-        //current working directory
         char cwd[1024];
-       
-        
         printf("/root%s> ", getenv("PWD"));
 
-        //taking in a command (maximum 100 words)
         size_t len = 100;
-        char* input =  malloc(len * sizeof(char));
+        char* input = malloc(len * sizeof(char));
+        if (fgets(input, len, input_source) == NULL) {
+            break; 
+        }
 
-
-        getline(&input,&len, stdin);
-        
-        //creating array of strings to store command (10 words maximum)
         size_t capacity = 10;
         char** words = malloc(capacity * sizeof(char*));
-    
+        char* input_file = NULL;
+        char* output_file = NULL;
+        int append_mode = 0;
+
+        int run_background = 0;
         char* token = strtok(input," \n");
-
-        //save each individual token(word) into an array
         size_t count = 0;
+        
         while (token != NULL){
-            words[count] = strdup(token);
-            count++;
+            if (strcmp(token, "<") == 0) {
+                token = strtok(NULL, " \n");
+                if (token) input_file = strdup(token);
+            } else if (strcmp(token, ">") == 0) {
+                token = strtok(NULL, " \n");
+                if (token) {
+                    output_file = strdup(token);
+                    append_mode = 0;
+                }
+            } else if (strcmp(token, ">>") == 0) {
+                token = strtok(NULL, " \n");
+                if (token) {
+                    output_file = strdup(token);
+                    append_mode = 1;
+                }
+
+            } else {
+                if(strcmp(token, "&") == 0){
+                    run_background = 1;
+                    token = NULL;
+                }
+                else{
+                    words[count] = strdup(token);
+                    count++;
+                }
+            }
             token = strtok(NULL, " \n");
+        }
+        words[count] = NULL;  
 
-    }
-
-
-    //checks the inputted command
         if(words[0] == NULL){
             printf("please input a command \n");
         }
-        else  if(strcmp(words[0],"ls") == 0 || strcmp(words[0], "dir") == 0){
-            if(words[1]){
-                list_files(words[1]);
+        else {
+            FILE* original_stdout = NULL;
+            FILE* new_stdout = NULL;
+            
+            if (output_file) {
+                original_stdout = stdout;
+                new_stdout = fopen(output_file, append_mode ? "a" : "w");
+                if (new_stdout) {
+                    stdout = new_stdout;
+                }
             }
-            else{
-                list_files(".");
-            }
-        }
-        else if (strcmp(words[0], "cd") == 0) {
-            if (words[1] == NULL) {
 
-                change_directory(NULL);
+            if(strcmp(words[0],"ls") == 0 || strcmp(words[0], "dir") == 0){
+                if(words[1]){
+                    list_files(words[1]);
+                }
+                else{
+                    list_files(".");
+                }
             }
-            else if(strcmp(words[1],".." ) == 0){
-                change_directory("..");
+            else if(strcmp(words[0], "cd") == 0) {
+                if (words[1] == NULL) {
+                    change_directory(NULL);
+                }
+                else if(strcmp(words[1],"..") == 0){
+                    change_directory("..");
+                }
+                else {
+                    change_directory(words[1]);
+                }
+            }
+            else if(strcmp(words[0],"exit") == 0){
+                if (output_file) {
+                    stdout = original_stdout;
+                    fclose(new_stdout);
+                    free(output_file);
+                }
+                if (input_file) free(input_file);
+                break;
+            }
+            else if(strcmp(words[0], "echo") == 0){
+                size_t index = 2;
+                char *current = words[1];
+                while(current != NULL){
+                    printf("%s ", current);
+                    current = words[index++];
+                }
+                printf("\n");
+            }
+            else if(strcmp(words[0],"set") == 0){
+                set(words[1], words[2]);
+            }
+            else if(strcmp(words[0], "environ") == 0){
+                environment();
+            }
+            else if(strcmp(words[0], "pause") == 0){
+                printf("please press enter to continue..");
+                while(getchar() != '\n'){
+                    continue;
+                }
             }
             else {
-                change_directory(words[1]);
+                pid_t pid = fork();
+                if(pid < 0) {
+                    perror("fork failed");
+                } else if(pid == 0) {
+                    if (input_file) {
+                        FILE* new_stdin = freopen(input_file, "r", stdin);
+                        if (!new_stdin) {
+                            perror("Failed to redirect input");
+                            exit(EXIT_FAILURE);
+                        }
+                    }
+                    if (output_file) {
+                        FILE* new_stdout = freopen(output_file, append_mode ? "a" : "w", stdout);
+                        if (!new_stdout) {
+                            perror("Failed to redirect output");
+                            exit(EXIT_FAILURE);
+                        }
+                    }
+                    if(execvp(words[0], words) == -1) {
+                        perror("command execution failed");
+                        exit(EXIT_FAILURE);
+                    }
+                } else {
+                    if(run_background == 0){
+                         waitpid(pid, NULL, 0);
+                    }
+                    else{
+                        printf("Running in background... \n");
+                        signal(SIGCHLD, SIG_IGN);
+                    }
+                }
             }
-        }
-        else if(strcmp(words[0],"exit") == 0){
-            break;
-        }
-        else if(strcmp(words[0], "echo") == 0){
-            size_t index = 2;
-            char *current = words[1];
-            while(current != NULL){
-                printf("%s ", current);
-                current =  words[index++];
+
+            if (output_file) {
+                fflush(stdout);
+                stdout = original_stdout;
+                fclose(new_stdout);
+                free(output_file);
             }
-            printf("\n");
-        }
-        else if(strcmp(words[0],"set") == 0){
-            set(words[1], words[2]);
-        }
-        else if(strcmp(words[0], "environ") == 0){
-            environment();
-        }
-        else{
-            execute_command(words);
         }
 
-    for(size_t i = 0; i < count;i++){
-        free(words[i]);
+        for(size_t i = 0; i < count; i++){
+            free(words[i]);
+        }
+        free(words);
+        free(input);
+        if (input_file) free(input_file);
     }
-    free(words);
-    free(input);
 }
+
+int main(int argc, char* argv[]){
+   
+    FILE* file; 
+    if (argc > 1) {
+        file = fopen(argv[1], "r");
+        if (file == NULL) {
+            perror("Could not open file");
+            exit(EXIT_FAILURE);
+        }
+        process_input(file);
+        printf("\n");
+        fclose(file);       
+    } else {
+        char *home = getenv("HOME");
+        if (home) {
+            setenv("PWD", home, 1);
+            chdir(home);
+        } else {
+            setenv("PWD", "/", 1);
+            chdir("/");
+        }
+        process_input(stdin);
+    }
+
+    return 0;
 
 }
